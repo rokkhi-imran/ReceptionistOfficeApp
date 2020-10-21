73d4a4877d1aa2357f90f7aaacdc6f463d2380c6
sk.rokkhi <sk.rokkhi@gmail.com>
2020-10-21 17:55:29 +0600

    created example api implementation for employee entry status

--- app/src/main/java/com/rokkhi/receptionistofficeapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/rokkhi/receptionistofficeapp/ui/main/MainViewModel.kt
@@ -1,28 +1,42 @@
 package com.rokkhi.receptionistofficeapp.ui.main
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.rokkhi.receptionistofficeapp.helper.SharedPrefHelper
 import com.rokkhi.receptionistofficeapp.network.wrapper.ApiResponse
+import com.rokkhi.receptionistofficeapp.networkmodel.EmployeeEntryChangeResponse
 import com.rokkhi.receptionistofficeapp.networkmodel.UserResponse
-import com.rokkhi.receptionistofficeapp.util.KeyFrame
-import timber.log.Timber
+import com.rokkhi.receptionistofficeapp.statics.EmployeeEntryStatus
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(private var repo: MainRepo, private val shf: SharedPrefHelper) : ViewModel() {
 
-    fun getUserByPhone(userPhoneNumber: String): LiveData<ApiResponse<UserResponse>> {
-        return Transformations.map(repo.getUserByPhone(userPhoneNumber)) {
-            when (it) {
-                is ApiResponse.Success -> {
-                    shf.putString(KeyFrame.NAME, it.data.data.name)
-                    Timber.e("userName from server: ${shf.getString(KeyFrame.NAME)}")
-                }
-            }
-            it
-        }
-    }
+
+    // *******
+    // note: apis can be called directly like below
+    // *******
+    fun getUserByPhone(userPhoneNumber: String): LiveData<ApiResponse<UserResponse>> = repo.getUserByPhone(userPhoneNumber)
+
+
+    // *******
+    // note: apis can be called with Transformations.map so that data can be manipulated inside view model
+    // *******
+//    fun getUserByPhone(userPhoneNumber: String): LiveData<ApiResponse<UserResponse>> {
+//        return Transformations.map(repo.getUserByPhone(userPhoneNumber)) {
+//            when (it) {
+//                is ApiResponse.Success -> {
+//                    shf.putString(KeyFrame.NAME, it.data.data.name)
+//                    Timber.e("userName from server: ${shf.getString(KeyFrame.NAME)}")
+//                }
+//                is ApiResponse.Progress -> {}
+//                is ApiResponse.Failure -> {}
+//                is ApiResponse.ErrorCode -> {}
+//            }
+//            it
+//        }
+//    }
+
+    fun recordEmployeeEntry(companyId: Int, employeeId: Int, departmentId: Int, branchId: Int, receptionistId: Int, acknowledgedBy: Int, status: EmployeeEntryStatus): LiveData<ApiResponse<EmployeeEntryChangeResponse>> = repo.recordEmployeeEntry(companyId, employeeId, departmentId, branchId, receptionistId, acknowledgedBy, EmployeeEntryStatus.INSIDE)
 
 
     override fun onCleared() {
